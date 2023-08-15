59a65fef938903903ab691ef5b1ee240f88c7750
NicolasAmato <nicolasamato@outlook.com>
2023-08-15 11:06:34 -0300

    camhiando extencion de html a php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css"> 
-</head>
-
-
-<body>
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra ">
-                <a class="logo-header" href="/">
-                    <img src="build/img/logo.svg" alt="logotipo Bienes Raices">
-                </a>
-                
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu resposive">
-                </div>
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="efecto-oscuro">
-
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div>        
-
-            
-            <h1>Venta de Casas y Departamentos Exclusivos</h1> 
-        
-        </div>
-    </header>
+<?php
+   include './includes/templates/header.php';
+?>
 
 
     <main class="contenedor seccion">
