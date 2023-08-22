ce9be858beb7e2d4c45d19c5f6752cb9950f013c
NicolasAmato <nicolasamato@outlook.com>
2023-08-22 10:55:55 -0300

    generando php/html

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,18 @@
+<?php
+     require 'includes/funciones.php';   
+     incluirTemplate('header');
+    ?>
+
+
+    <main class="contenedor seccion">
+        <h1> BASE</h1>
+    </main>
+
+
+    <?php
+        
+        incluirTemplate('footer');
+       ?>
+   
+</body>
+</html>
