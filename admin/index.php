9d4a1b964d609a419ef9396f03f775ccf87e97f8
NicolasAmato <nicolasamato@outlook.com>
2023-08-22 11:22:52 -0300

    cambios ok

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@
     <main class="contenedor seccion">
         <h1> Administrador </h1>
 
-        <a href="../admin/propiedades/crear.php" class="boton boton-verde-no-block"> Nueva Propiedad </a>
+        <a href="/BienesRaices/admin/propiedades/crear.php" class="boton boton-verde-no-block"> Nueva Propiedad </a>
     </main>
 
 
