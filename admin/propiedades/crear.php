9df64d7298ba988f16f0ca3df092d659a28daef9
NicolasAmato <nicolasamato@outlook.com>
2023-08-22 14:41:32 -0300

    generando form de creacion de publicacions

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -8,7 +8,29 @@
         <h1> crear </h1>
 
 
-        <a href="BienesRaices/admin/index.php" class="boton boton-verde-no-block">volver</a>
+        <a href="/bienesRaices/admin/index.php" class="boton boton-verde-no-block">volver</a>
+
+
+        <form class="formulario">
+            <fieldset>
+              <legend> Informacion General </legend>
+
+              <label for="titulo">titulo</label>
+              <input type="text" id="titulo" placeholder="nombre propiedad">
+
+              <label for="precio"> Precio: </label>
+              <input type="text" id="precio" placeholder="nombre propiedad">
+
+              <label for="imagen"> imagen </label>
+              <input type="file" id="imagen">
+
+            </fieldset>
+            
+
+
+
+
+        </form>
     </main>
 
 
