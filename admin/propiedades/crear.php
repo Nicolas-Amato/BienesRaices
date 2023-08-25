ccfddf4bf674ee37f4c21fbb1b3932e6f2096d92
NicolasAmato <nicolasamato@outlook.com>
2023-08-25 12:21:43 -0300

    manteniendo datos ingresados si falta un campo, OK

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,6 +3,14 @@
   require '../../includes/config/database.php';
   $DB = conectar_DB();
 
+
+    $titulo = '';
+    $precio = '';
+    $descipcion = '';
+    $habitaciones = '';
+    $WC= '';
+    $estacionamiento = '';
+    $vendedor_ID = '';
   
   if($_SERVER['REQUEST_METHOD']  === 'POST' ) {
 
@@ -61,11 +69,11 @@
 
         <a href="/bienesRaices/admin/index.php" class="boton boton-verde-no-block">volver</a>
 
-        <?php foreach($errores as $error):?>
-         <div class="alerta error">
-          <?php echo $error; ?>
-         </div>
-      <?php endforeach; ?>
+       <?php foreach($errores as $error): ?>
+       <div class="alerta error">
+        <?php echo $error; ?>
+       </div>
+       <?php endforeach; ?>
 
 
         <form class="formulario" method="POST" action="/bienesraices/admin/propiedades/crear.php">
@@ -73,16 +81,16 @@
               <legend> Informacion General </legend>
 
               <label for="titulo">titulo</label>
-              <input type="text" id="titulo" name='titulo' placeholder="nombre propiedad">
+              <input type="text" id="titulo" name='titulo' placeholder="nombre propiedad" value="<?php echo $titulo; ?>">
 
               <label for="precio"> Precio: </label>
-              <input type="NUMBER" id="precio" name='precio' placeholder="preciod propiedad" min="1">
+              <input type="NUMBER" id="precio" name='precio' placeholder="preciod propiedad" min="1" value="<?php echo $precio; ?>">
 
               <label for="imagen"> imagen </label>
-              <input type="file" id="imagen" name='imagen' accept="image/jpg , image/png">
+              <input type="file" id="imagen" name='imagen' accept="image/jpg , image/png" >
 
               <label for="descipcion"> descripcion </label>
-              <textarea id="descipcion" name="descipcion" ></textarea>
+              <textarea id="descipcion" name="descipcion" >"<?php echo $descipcion; ?>"</textarea>
 
             </fieldset>
             
@@ -91,13 +99,13 @@
               <legend>Informacion Propiedad</legend>
 
               <label for="habitaciones">habitaciones</label>
-              <input type="number" id="habitaciones" name='habitaciones' laceholder="cantidad de habitaciones" min="1" max="9">
+              <input type="number" id="habitaciones" name='habitaciones' laceholder="cantidad de habitaciones" min="1" max="9" value="<?php echo $habitaciones; ?>">
  
               <label for="WC">baños</label>
-              <input type="number" id="WC" name='WC' placeholder="cantidad de baños" min="1" max="9">
+              <input type="number" id="WC" name='WC' placeholder="cantidad de baños" min="1" max="9" value="<?php echo $WC;?>">
               
-              <label for="estacionamiento">estacionamiento</label>
-              <input type="number" id="estacionamiento" name='estacionamiento' placeholder="cantidad de coches" min="1" max="9">
+              <label for="estacionamiento">estacionamiento:</label>
+              <input type="number" id="estacionamiento" name='estacionamiento' placeholder="ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
        
             </fieldset>
 
