039d950a027c964c5a77435c0b0c81e3c16ff1f0
NicolasAmato <nicolasamato@outlook.com>
2023-09-04 12:03:24 -0300

    cambios en scss error

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,39 @@
         <?php endif;?>
 
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde-no-block"> Nueva Propiedad </a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <th>
+                    <tr>1</tr>
+                    <tr>casa en la playa</tr>
+                    <tr> <img src="../imagen/1f2bbaab32e878c781145e0665ef4b5a.jpg" class="imagen-repo"></tr>
+                    <tr>$ 252000</tr>
+                    <tr>
+                       <a href="">eliminar</a>
+                       <a href="">actualizar</a>
+                    </tr>
+                </th>
+                
+        
+            </tbody>
+        </table>
+
+
+
+
+
+
+
     </main>
 
 
