81f1651096e9f8bb1482d4ac86eeb82c9a58d5c3
NicolasAmato <nicolasamato@outlook.com>
2023-09-13 15:37:20 -0300

    anuncios.ok

---
+++ includes/template/anuncios.php
@@ -0,0 +1,54 @@
+<?php
+//importar conexcion basa datos
+require 'includes/config/database.php';
+$DB = conectar_DB();
+
+//conaultar base de datos
+$query = "SELECT * FROM propiedades";
+
+//obtenes resultado
+$resuldatoAnuncio = mysqli_query($DB, $query);
+
+?>
+
+<div class="contenedor-anuncio">
+  <?php while($propiedad = mysqli_fetch_assoc($resuldatoAnuncio)): ?>
+      <div class="anuncio">
+         <div class="reacomodar">
+          <img class="acomodar-img" loading="lazy" src="/imagen/<?php echo $propiedad['imagen']; ?>" alt="imagen anuncio">
+         </div>        
+        
+         <div class="contenido-anuncio">
+
+         <h3><?php echo $propiedad['titulo']; ?></h3>
+         <?php 
+            $cantidad=50;
+            $descripciones = $propiedad['descipcion']; 
+            $descripcion = (strlen($descripciones)>$cantidad) ? substr($descripciones,0,$cantidad).'...' : $descripciones;?>
+          <p><?php echo $descripcion;?></p>
+          <p class="precio"><?php echo number_format($propiedad['precio']); ?></p>
+    
+          <ul class="icono-caracteristicas">
+            <li>
+               <img loading="lazy" src="build/img/icono_wc.svg" alt="icono anuncio">
+               <p><?php echo $propiedad['WC']; ?></p>
+            </li>
+            <li>
+               <img loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+               <p><?php echo $propiedad['estacionamiento']; ?></p>
+            </li>
+            <li>
+               <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
+               <p><?php echo $propiedad['habitaciones']; ?></p>
+            </li>
+          </ul>
+
+          <a href="anuncios?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo">
+           ver propiedad
+          </a>
+         </div>
+      </div>
+   <?php endwhile; ?>
+
+</div>
+
