936331796fcd2b852fa3dbd3a21911d9b5746b50
NicolasAmato <nicolasamato@outlook.com>
2023-09-14 15:53:59 -0300

    todo ok anuncio.php

--- anuncio.php
+++ anuncio.php
@@ -19,21 +19,16 @@
     //obtenes resultado
     $resuldatoAnuncio = mysqli_query($DB, $query);
     $propiedad = mysqli_fetch_assoc($resuldatoAnuncio);
-
-    echo "<pre>";
-    var_dump($propiedad);
-    echo "<pre>";
-
 ?>
 
- <main class="contenedor seccion contenido-centrado">
+<main class="contenedor seccion contenido-centrado">
         <h1><?php echo $propiedad['titulo']; ?></h1>
         <img  loading="lazy" src="/BienesRaices/imagen/<?php echo $propiedad['imagen'];?>" alt="imagen anuncio">
-        <div class="resumen-propiedad">
+         
+    <div class="resumen-propiedad">
 
-            <p class="precio"><?php echo number_format($propiedad['precio']); ?></p>
-
-            <ul class="icono-caracteristicas">
+        <p class="precio"><?php echo number_format($propiedad['precio']); ?></p>
+        <ul class="icono-caracteristicas">
             <li>
                 <img loading="lazy" src="build/img/icono_wc.svg" alt="icono anuncio">
                 <p><?php echo $propiedad['WC']; ?></p>
@@ -46,16 +41,16 @@
                 <img loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
                 <p><?php echo $propiedad['habitaciones']; ?></p>
             </li>
-           </ul>
+        </ul>
            
-           <p><?php echo $descripciones = $propiedad['descipcion']; ?></p>
+        <p><?php echo $descripciones = $propiedad['descipcion']; ?></p>
 
 
           
         
-        </div>
+       </div>
             
- </main>
+</main>
 
 <?php
 incluirTemplate("footer");
